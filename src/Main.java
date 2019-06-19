4a22cc4286be1d3d6392c87969f285596f2818f1
Яна Демкив <studioarttt@gmail.com>
2019-06-19 22:04:58 +0400

    simple_1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.sql.SQLOutput;
 import java.util.*;
 
 
@@ -15,32 +16,32 @@ public class Main {
 
         //System.out.println( RomanNumerals(300));
 
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("Введите число a + b");
-        String s = reader.readLine();
-        int a = 0;
-        int b = 0;
-        int c = 0;
-        int y = 0;
-        String d = s.replace(" ","");
-        d.split("+");
-        for (int i = 0; i < d.length(); i++) {
-            System.out.println(d.charAt(i));
-
-        }
-
-        int h = 0;
-        String newIntA = "";
-        for (int i = 0; i < d.length(); i++) {
-            if (d.charAt(i) -'0' == h) {
-                newIntA +=d.charAt(i)-'0';
-            } else {
-                h++;
-            }
-
-        }
-        System.out.println("Новая строка");
-        System.out.println(newIntA);
+//        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+//        System.out.println("Введите число a + b");
+//        String s = reader.readLine();
+//        int a = 0;
+//        int b = 0;
+//        int c = 0;
+//        int y = 0;
+//        String d = s.replace(" ","");
+//        d.split("+");
+//        for (int i = 0; i < d.length(); i++) {
+//            System.out.println(d.charAt(i));
+//
+//        }
+//
+//        int h = 0;
+//        String newIntA = "";
+//        for (int i = 0; i < d.length(); i++) {
+//            if (d.charAt(i) -'0' == h) {
+//                newIntA +=d.charAt(i)-'0';
+//            } else {
+//                h++;
+//            }
+//
+//        }
+//        System.out.println("Новая строка");
+//        System.out.println(newIntA);
         //System.out.println("метод replace");
         // System.out.println(s.replace(" ",""));
 
@@ -48,12 +49,95 @@ public class Main {
 //            System.out.print("Ответ: ");
 //            System.out.println(answerInt(a,b));
 //        }
+        //ввод данных
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String s = reader.readLine();
+        //убрать все пробелы из введеной строки
+        String d = s.replace(" ","");
+
+        //проверка арифметической операции (содержит ли строка нужный символ)
+        String operattion = "";
+        if (s.contains("+")) {
+            operattion = "\\+";
+        } else if (s.contains("-")){
+            operattion = "-";
+        } else if (s.contains("*")) {
+            operattion = "\\*";
+        } else if (s.contains("/")) {
+            operattion = "/";
+        } else {
+            System.out.println("Несоответсвующая арифметическая операция.");
+        }
+
+        //разделение строки по арифметич. знаку, добавить иф для разных арифметических знаков
+        String [] parts = d.split(operattion);
+
+        int o = parts[0].length(); //длинна строки элемента массива 0(первая часть строки)
+        int t = parts[1].length(); //длинна строки элемента массива 1(вторая часть строки после символа)
+
+
+
+        String f = parts[0]; //присваеваем строке элемент массива 0
+        String h = parts[1]; //присваеваем строке элемент массива 1
+
+        //проверка символов на наличие цифр в элемекнте массива строк
+//        for (int i = 0; i < o; i++) {
+//            f.charAt(i);
+//        }
+
+        //объявили переменные
+        int u = 0;
+        int w = 0;
+        //исключение на случай введеных букв
+        try {
+            u = Integer.parseInt(f);
+            w = Integer.parseInt(h);
+            System.out.println(u);
+            System.out.println(w);
+        } catch (Exception e) {
+            System.out.println("В введенных данных присутсвует буквенные символы.");
+        }
+
+
+        //вызов метода арифметической операции
+        if (operattion.contains("+")) {
+            System.out.println(answerInt(u, w));
+        } else if (operattion.contains("-")){
+            System.out.println(answerInt4(u, w));
+        } else if (operattion.contains("*")) {
+            System.out.println(answerInt2(u, w));
+        } else if (operattion.contains("/")) {
+            System.out.println(answerInt3(u, w));
+        } else {
+            System.out.println("Несоответсвующая арифметическая операция.");
+        }
+
+
+
     }
 
+
+    //арифметические методы
     public static int answerInt(int a, int b){
 
         return a + b;
     }
+
+    public static int answerInt2(int a, int b){
+
+        return a * b;
+    }
+
+    public static int answerInt3(int a, int b){
+
+        return a / b;
+    }
+
+    public static int answerInt4(int a, int b){
+
+        return a - b;
+    }
+    //римс циф
 //    public static String RomanNumerals(int Int) {
 //        LinkedHashMap<String, Integer> roman_numerals = new LinkedHashMap<String, Integer>();
 //        roman_numerals.put("M", 1000);
