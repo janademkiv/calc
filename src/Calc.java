407f486cf972659b971f9f1c392884902cdf3d8c
Яна Демкив <studioarttt@gmail.com>
2019-06-21 15:48:48 +0400

    final_2

--- src/Calc.java
+++ src/Calc.java
@@ -2,17 +2,20 @@ import java.util.regex.Pattern;
 
 abstract class Calc {
 
+    //
     public int calculate(String expression) {
 
+        //получаем символ арифметической операции
         String operation = getOperation(expression);
 
         //убрать все пробелы из введеной строки
-        //разделение строки по арифметич. знаку, добавить иф для разных арифметических знаков
+        //разделение строки по арифметич. знаку
         String [] parts = expression.replace(" ","").split(Pattern.quote(operation));
 
         int a = strToInt(parts[0]); //присваеваем строке элемент массива 0
         int b = strToInt(parts[1]); //присваеваем строке элемент массива 1
 
+        // исходя из полученного символа, вызывается нажная нам арифм. операц.
         switch (operation) {
             case "+":
                 return plus(a, b);
@@ -27,6 +30,7 @@ abstract class Calc {
         }
     }
 
+    //проверяем, содержит ли введеная строка символ арифметической операции
     protected String getOperation(String expression){
         if (expression.contains("+")) {
             return "+";
@@ -39,6 +43,8 @@ abstract class Calc {
         } else {
             throw new RuntimeException("Несоответсвующая арифметическая операция.");
         }
+
+        
     }
 
     protected int plus(int a, int b){
@@ -61,6 +67,8 @@ abstract class Calc {
         return a - b;
     }
 
+
+    //абстрактный метод для конвертации данных.
     protected abstract int strToInt(String s);
     protected abstract String intToStr(int s);
 }
